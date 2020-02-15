774242451ddb80c07250ecb0137038614a301df1
ZoraizQ <21100130@lums.edu.pk>
2020-02-15 16:06:19 +0500

    learnmore pug file updated

--- controllers/api.js
+++ controllers/api.js
@@ -1,20 +1,6 @@
-const { promisify } = require('util');
-const cheerio = require('cheerio');
 const graph = require('fbgraph');
-const { LastFmNode } = require('lastfm');
-const tumblr = require('tumblr.js');
-const { Octokit } = require('@octokit/rest');
-const Twit = require('twit');
-const stripe = require('stripe')(process.env.STRIPE_SKEY);
-const twilio = require('twilio')(process.env.TWILIO_SID, process.env.TWILIO_TOKEN);
-const clockwork = require('clockwork')({ key: process.env.CLOCKWORK_KEY });
-const paypal = require('paypal-rest-sdk');
-const lob = require('lob')(process.env.LOB_KEY);
-const ig = require('instagram-node').instagram();
-const axios = require('axios');
 const { google } = require('googleapis');
 const Quickbooks = require('node-quickbooks');
-const validator = require('validator');
 
 Quickbooks.setOauthVersion('2.0');
 
--- controllers/learnmore.js
+++ controllers/learnmore.js
@@ -1,6 +1,3 @@
-const validator = require('validator');
-const nodemailer = require('nodemailer');
-
 /**
  * GET /learnmore
  * Contact form page.
@@ -12,82 +9,4 @@ exports.getLearnmore = (req, res) => {
     title: 'Learn More',
     unknownUser,
   });
-};
-
-/**
- * POST /learnmore
- * Send a learnmore form via Nodemailer.
- */
-exports.postLearnmore = (req, res) => {
-  const validationErrors = [];
-  let fromName;
-  let fromEmail;
-  if (!req.user) {
-    if (validator.isEmpty(req.body.name)) validationErrors.push({ msg: 'Please enter your name' });
-    if (!validator.isEmail(req.body.email)) validationErrors.push({ msg: 'Please enter a valid email address.' });
-  }
-  if (validator.isEmpty(req.body.message)) validationErrors.push({ msg: 'Please enter your message.' });
-
-  if (validationErrors.length) {
-    req.flash('errors', validationErrors);
-    return res.redirect('/learnmore');
-  }
-
-  if (!req.user) {
-    fromName = req.body.name;
-    fromEmail = req.body.email;
-  } else {
-    fromName = req.user.profile.name || '';
-    fromEmail = req.user.email;
-  }
-
-  let transporter = nodemailer.createTransport({
-    service: 'SendGrid',
-    auth: {
-      user: process.env.SENDGRID_USER,
-      pass: process.env.SENDGRID_PASSWORD
-    }
-  });
-  const mailOptions = {
-    to: 'your@email.com',
-    from: `${fromName} <${fromEmail}>`,
-    subject: 'Contact Form | Hackathon Starter',
-    text: req.body.message
-  };
-
-  return transporter.sendMail(mailOptions)
-    .then(() => {
-      req.flash('success', { msg: 'Email has been sent successfully!' });
-      res.redirect('/learnmore');
-    })
-    .catch((err) => {
-      if (err.message === 'self signed certificate in certificate chain') {
-        console.log('WARNING: Self signed certificate in certificate chain. Retrying with the self signed certificate. Use a valid certificate if in production.');
-        transporter = nodemailer.createTransport({
-          service: 'SendGrid',
-          auth: {
-            user: process.env.SENDGRID_USER,
-            pass: process.env.SENDGRID_PASSWORD
-          },
-          tls: {
-            rejectUnauthorized: false
-          }
-        });
-        return transporter.sendMail(mailOptions);
-      }
-      console.log('ERROR: Could not send learnmore email after security downgrade.\n', err);
-      req.flash('errors', { msg: 'Error sending the message. Please try again shortly.' });
-      return false;
-    })
-    .then((result) => {
-      if (result) {
-        req.flash('success', { msg: 'Email has been sent successfully!' });
-        return res.redirect('/learnmore');
-      }
-    })
-    .catch((err) => {
-      console.log('ERROR: Could not send learnmore email.\n', err);
-      req.flash('errors', { msg: 'Error sending the message. Please try again shortly.' });
-      return res.redirect('/learnmore');
-    });
 };
