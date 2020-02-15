fdb46998696af7fe26a0583da292d68748d5bd6d
saadullah01 <61075964+saadullah01@users.noreply.github.com>
2020-02-15 23:58:04 +0500

    Pictures + Messaging App

--- public/js/main.js
+++ public/js/main.js
@@ -1,11 +1,12 @@
 $(document).ready(() => {
+
 });
 
 new Vue({
   template:`
     <div class="container-fluid w-100 h-100">
       <div class="row justify-content-center h-100">
-        <div class="col-sm-12 col-md-8 col-xl-12 chat">
+        <div class="col-sm-12 col-md-8 col-xl-10 chat">
           <div class="card">
             <!-- Top Profile -->
             <div class="card-header msg_head">
@@ -20,15 +21,29 @@ new Vue({
               </div>
             </div>
             <!-- Messages -->
-            <div class="card-body msg_card_body">
-              <!-- Same Message in For Loop -->
-              <div class="d-flex justify-content-start mb-4">
-                <div class="img_cont_msg">
-                  <img src="robot.jpeg" class="rounded-circle user_img_msg">
+            <div v-if="preProcessing()" class="card-body msg_card_body">
+              <!-- ChatBot Message in For Loop -->
+              <div v-for="m in messages">
+                <div v-if="changeIt(botTurn)" class="d-flex justify-content-start mb-4">
+                  <div class="img_cont_msg">
+                    <img src="robot.jpeg" class="rounded-circle user_img_msg">
+                  </div>
+                  <div class="msg_cotainer">
+                    <div v-if="m['type']">{{m['data']}}</div>
+                    <div v-else><img v-bind:src="m['data']" class="pic_msg"></div>
+                    <span class="msg_time">8:40 AM</span>
+                  </div>
                 </div>
-                <div class="msg_cotainer">
-                  {{messages[0]}}
-                  <span class="msg_time">8:40 AM, Today</span>
+                <!-- User Message -->
+                <div v-else class="d-flex justify-content-end mb-4">
+                  <div class="msg_cotainer_send">
+                    <div v-if="m['type']">{{m['data']}}</div>
+                    <div v-else><img v-bind:src="m['data']" class="pic_msg"></div>
+                    <span class="msg_time_send">8:55 AM</span>
+                  </div>
+                  <div class="img_cont_msg">
+                    <img src="user.png" class="rounded-circle user_img_msg">
+                  </div>
                 </div>
               </div>
             </div>
@@ -41,12 +56,23 @@ new Vue({
     </div>
   `,
   data:{
-    chatBotPic: 'robot.jpeg',
-    messages: ['Hi, How are you?'],
-    userPic: 'user.png',
-
+    messages: [{'type':1, 'data':'Hi, How are you?'}, {'type':0, 'data':'robot.jpeg'}, {'type':0, 'data':'user.png'}],
+    botTurn: 1
   },
   methods:{
-
+    preProcessing(){
+      this.botTurn = 1;
+      return true
+    },
+    changeIt(inp){
+      if(this.botTurn){
+        this.botTurn = 0
+        return true
+      }
+      else{
+        this.botTurn = 1
+        return false
+      }
+    }
   }
 }).$mount('#root')
